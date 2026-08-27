27e865e59b6457ff9be27e2c7259bd683bf75ffe
sauraavpdl <283926817+sauraavpdl@users.noreply.github.com>
2026-08-27 15:16:51 +0545

    Add dark mode/light mode toggle button and make it functional

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+// hamburger menu
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', function() {
     navLinks.classList.toggle('active');
+});
+
+
+// theme toggle
+const themeToggle = document.querySelector('.theme-toggle');
+
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+}
+
+themeToggle.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
 });
